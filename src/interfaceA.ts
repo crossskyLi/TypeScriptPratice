b9d4aceefa698097e42a168cb190df1e6946a864
liguanliang <liguanliang@sensetime.com>
2020-01-10 14:39:54 +0800

    feat: interface

--- src/interfaceA.ts
+++ src/interfaceA.ts
@@ -1,27 +1,27 @@
 (() => {
 
-  function printLabel(labelledObj: { label: string }) {
-    console.log(labelledObj.label);
-  }
+  // function printLabel(labelledObj: { label: string }) {
+  //   console.log(labelledObj.label);
+  // }
 
-  let myObj = { size: 10, label: "Size 10 Object" };
-  printLabel(myObj);
+  // let myObj = { size: 10, label: "Size 10 Object" };
+  // printLabel(myObj);
 
   /* readonly */
-  interface ReadonlyPerson {
-    readonly name: string;
-  }
-  const person: ReadonlyPerson = {
-    name: "George"
-  }
+  // interface ReadonlyPerson {
+  //   readonly name: string;
+  // }
+  // const person: ReadonlyPerson = {
+  //   name: "George"
+  // }
 
   /* error */
   // person.name = 222;
 
 
   /* ReadonlyArray<T> */
-  const readonlyArr: number[] = [1, 55, 5];
-  const readonlyArrExp: ReadonlyArray<number> = readonlyArr;
+  // const readonlyArr: number[] = [1, 55, 5];
+  // const readonlyArrExp: ReadonlyArray<number> = readonlyArr;
   /* error */
   // readonlyArrExp[1] = 1
 
@@ -112,24 +112,145 @@
 
 
   /* 类类型 */
-  interface IClock {
-    currentTime: Date;
-    setTime(time: Date): Date
-  }
+  // interface IClock {
+  //   currentTime: Date;
+  //   setTime(time: Date): Date
+  // }
 
-  class Clock implements IClock {
+  // class Clock implements IClock {
 
-    currentTime: Date;
+  //   currentTime: Date;
 
-    setTime(time: Date): Date {
-      this.currentTime = time;
-      return time;
-    };
+  //   setTime(time: Date): Date {
+  //     this.currentTime = time;
+  //     return time;
+  //   };
 
-    constructor() {
-      this.currentTime = new Date()
-    }
-  }
+  //   constructor() {
+  //     this.currentTime = new Date()
+  //   }
+  // }
+
+  // 当你操作类和接口的时候，你要知道类是具有两个类型的：静态部分的类型和实例的类型。 
+  // 你会注意到，当你用构造器签名去定义一个接口并试图定义一个类去实现这个接口时会得到一个错误：
+  // 这里因为当一个类实现了一个接口时，只对其实例部分进行类型检查。 
+  // constructor存在于类的静态部分，所以不在检查的范围内。
+  // interface ClockConstructor {
+  //   new(hour: number, minute: number);
+  // }
+
+  // class Clock implements ClockConstructor {
+  //   currentTime: Date;
+  //   constructor(hour: number, minute: number) { }
+  // }
+  // const clock = new Clock(12, 0)
+
+  // 我们应该直接操作类的静态部分。 看下面的例子，我们定义了两个接口， 
+  // ClockConstructor为构造函数所用和ClockInterface为实例方法所用。 
+  // 为了方便我们定义一个构造函数 createClock，它用传入的类型创建实例。
+
+  // interface ClockInterface {
+  //   tick(): any;
+  // }
+  // interface ClockConstructor {
+  //   new(hour: number, minute: number): ClockInterface;
+  // }
+
+  // class DigitalClock implements ClockInterface {
+  //   constructor(hour: number, minute: number) {
+
+  //   }
+  //   tick() {
+  //     console.log('beep')
+  //   }
+  // }
+
+  // class AnalogClock implements ClockInterface {
+  //   constructor(hour: number, minute: number) {
+
+  //   }
+  //   tick() {
+  //     console.log('tick')
+  //   }
+  // }
+
+  // function creatClock(Ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+  //   return new Ctor(hour, minute)
+  // }
+
+  /**
+   * 继承接口
+   * 
+   * 可以继承多个接口
+   *  */
+  // interface Fruit {
+  //   color: string;
+  // }
+
+  // interface Food {
+  //   calories: number;
+  // }
+
+  // interface Apple extends Fruit, Food {
+  //   weight: number;
+  // }
+
+  // let apple: Apple = {
+  //   color: 'red',
+  //   weight: 300,
+  //   calories: 500
+  // }
+
+  /* 混合类型 */
+  // interface Counter {
+  //   (start: number): string;
+  //   interval: number;
+  //   reset(): void;
+  // }
+
+  // function getCounter(interval: number): Counter {
+  //   const counter = <Counter>function (start: number) {
+  //     this.start = 2222;
+  //   }
+  //   counter.interval = interval;
+  //   counter.reset = function () {
+  //     this.interval = 0;
+  //   }
+  //   return counter;
+  // }
+
+  // let counter = getCounter(222)
+
+  // counter(10);
+  // counter.reset();
+  // counter.interval = 5.0;
+
+
+  // class Control {
+  //   private state: any;
+  // }
+
+  // interface SelectableControl extends Control {
+  //   select(): void
+  // }
+
+  // class Button extends Control implements SelectableControl {
+  //   select() {
+  //     console.log(1)
+  //   }
+  // }
+
+  // class TextBox extends Control {
+  //   select() {
+  //     return 1
+  //   }
+  // }
+  // /* error 缺少了 state */
+  // class Image implements SelectableControl {
+  //   select() {
+  //     return 1
+  //   }
+  // }
 
 })()
 
