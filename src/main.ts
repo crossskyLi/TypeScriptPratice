0fcdb4cb20342c94f430be40fbdfdac402d3fb60
liguanliang <liguanliang@sensetime.com>
2020-01-14 15:29:54 +0800

    feat: generic

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import interfaceA from "./interfaceA"
 import genericType from "./genericType"
 import "./classExample"
 import "./functionExample"
+import "./genericExample"
 
 function main(): void {
   console.log(interfaceA)
